61b2ceaadb4cbb04f1483661ba0a025c4a207f6f
Felicia Bengtsson <bengtssonfelicia@gmail.com>
2025-03-20 15:03:10 +0100

    lagt till nav

--- src/components/NavigationTop.tsx
+++ src/components/NavigationTop.tsx
@@ -3,8 +3,11 @@ function NavigationTop() {
   return (
 	<> 
 		<nav>
-			<div>
-				<a href="/">Hem</a>
+			<div className="nav-links">
+				<a href="/"><img id="logo-nav" src="/logo.webp" alt="logo" /></a>
+				<a href="#"><span className="material-symbols-outlined">search</span></a>
+				<a href="#"><span></span></a>
+				<a href="#"><span></span></a>
 			</div>
 		</nav>
 	</>  
